60cb3a568c7f245046f1da520e1a6be67d8e9e89
Lizzy <lb.mailbox@proton.com>
2022-12-16 21:39:53 +0100

    we can now cut a chunk of beef into slices!

--- tools/Knife.java
+++ tools/Knife.java
@@ -1,6 +1,10 @@
 package tools;
 
 
-public class Knife {
+public class Knife { // insert methods with actions you can do with a knife on ingredients
+    void Slice(ICuttable cuttable){
+        
+    }
 
+    //void Dice(IDiceable dicable){} // this interface does not exist yet
 }
