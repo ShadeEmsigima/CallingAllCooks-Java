e72589d2f2b5eb1d14ab6d469d33fa2b75dca74c
Lizzy <lb.mailbox@proton.com>
2022-12-16 23:32:28 +0100

    WIP slice mechanic

--- CallingAllCooks-Java.java
+++ CallingAllCooks-Java.java
@@ -1,17 +1,25 @@
 import ingredients.*;
+import tools.*;
 
 class RunCAC{
 
     public static void main(String[] args) {
-        Start();
+        {
+            Start();
+            Knife knife = new Knife(); // should I make this a singleton?
+        }
 
         Beef beef = new Beef();
-        beef.Cut();
+        
+        Knife.Slice(beef);
+        //beef.Slice();
         
         Stop();
     }
 
     static void Start(){
+
+        
         System.out.println("======================= \n");
         
         System.out.println("Welcome to Calling all Cooks!" + "\n");
--- tools/ICuttable.java
+++
@@ -1,6 +0,0 @@
-package tools;
-
-
-public interface ICuttable {
-    void Cut();
-}
